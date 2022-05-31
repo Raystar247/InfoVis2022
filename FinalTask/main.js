b610dcb007330010d70e0433b2ff9c1a31347a09
Raystar247 <24star10@gmail.com>
2022-05-31 15:30:04 +0900

    add japan.geojson

--- FinalTask/main.js
+++ FinalTask/main.js
@@ -1,6 +1,13 @@
-d3.csv("data.csv")
+d3.csv("https://raystar247.github.io/InfoVis2022/FinalTask/data.csv")
     .then( data => {
-        data.ForEach( d => { d.x = +d.month_wage; d.y = +d.n_starbucks; })
+        data.forEach( d => { d.x = +d.month_wage; d.y = +d.n_starbucks; d.r = 2; });
+        let config = {
+            parent: "#drawing_region_scatter_plot",
+            width: 360,
+            height: 360
+        };
+        let scatter_plot = new ScatterPlot( config, data );
+        scatter_plot.update();
     })
     .catch( error => {
         console.log( error );
